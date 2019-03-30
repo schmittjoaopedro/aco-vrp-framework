9d5f4fe1f5ee4aace87d0459ca94a50dc2a776dc
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2019-03-29 21:16:16 -0300

    - Add sample test

--- src/test/java/com/github/schmittjoaopedro/vrp/mpdptw/MPDPTW_MMAS_TEST.java
+++ src/test/java/com/github/schmittjoaopedro/vrp/mpdptw/MPDPTW_MMAS_TEST.java
@@ -1,15 +1,17 @@
 package com.github.schmittjoaopedro.vrp.mpdptw;
 
+import com.github.schmittjoaopedro.tsp.utils.Maths;
 import com.github.schmittjoaopedro.vrp.dvrptw.DVRPTW_ACS_Test;
 import org.junit.Test;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class MPDPTW_MMAS_TEST {
 
     private static final String rootDirectory;
 
-    private static int statisticInterval = 1;
+    private static int statisticInterval = 100;
 
     private static int maxIterations = 1000;
 
@@ -20,8 +22,61 @@ public class MPDPTW_MMAS_TEST {
     }
 
     @Test
+    public void mpdptw_main_problems_test() {
+        executeTest("l_4_25_1.txt");
+        executeTest("l_8_25_1.txt");
+        executeTest("n_4_25_1.txt");
+        executeTest("n_8_25_1.txt");
+        executeTest("w_4_25_1.txt");
+        executeTest("w_8_25_1.txt");
+        executeTest("l_4_50_1.txt");
+        executeTest("l_8_50_1.txt");
+        executeTest("n_4_50_1.txt");
+        executeTest("n_8_50_1.txt");
+        executeTest("w_4_50_1.txt");
+        executeTest("w_8_50_1.txt");
+        executeTest("l_4_100_1.txt");
+        executeTest("l_8_100_1.txt");
+        executeTest("n_4_100_1.txt");
+        executeTest("n_8_100_1.txt");
+        executeTest("w_4_100_1.txt");
+        executeTest("w_8_100_1.txt");
+        executeTest("l_4_400_1.txt");
+        executeTest("l_8_400_1.txt");
+        executeTest("n_4_400_1.txt");
+        executeTest("n_8_400_1.txt");
+        executeTest("w_4_400_1.txt");
+        executeTest("w_8_400_1.txt");
+
+    }
+
+    private void executeTest(String problem) {
+        ArrayList<Double> costs = new ArrayList<>();
+        ArrayList<Double> time = new ArrayList<>();
+        int sampleSize = 5;
+        int feasible = 0;
+        for (int i = 0; i < sampleSize; i++) {
+            Solver solver = new Solver(rootDirectory, problem, maxIterations, i, 0.02, statisticInterval, true);
+            solver.run();
+            costs.add(solver.getBestSolution().totalCost);
+            time.add(solver.getGlobalStatistics().getTimeStatistics().get("Algorithm").doubleValue());
+            if(solver.getBestSolution().feasible) {
+                feasible++;
+            }
+        }
+        double meanCosts = Maths.getMean(costs);
+        double meanTime = Maths.getMean(time);
+        System.out.println("---------------------------------------");
+        System.out.println("Instance = " + problem);
+        System.out.println("Feasible = " + feasible +  " of " + sampleSize);
+        System.out.println("Mean costs = " + meanCosts);
+        System.out.println("Mean time = " + meanTime);
+        System.out.println("---------------------------------------");
+    }
+
+    @Test
     public void mpdptw_large_4_25_1_test() {
-        Solver solver = new Solver(rootDirectory, "l_4_400_1.txt", maxIterations, seed, 0.02, statisticInterval, true);
+        Solver solver = new Solver(rootDirectory, "n_8_25_1.txt", maxIterations, seed, 0.02, statisticInterval, true);
         solver.run();
     }
 
