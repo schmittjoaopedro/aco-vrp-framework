e82b7be8f7be9347b89d98c33b2c29eb007f40bd
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2019-04-09 20:28:28 -0300

    Add route printer to ALNS

--- src/test/java/com/github/schmittjoaopedro/vrp/mpdptw/MPDPTW_ALNS_TEST.java
+++ src/test/java/com/github/schmittjoaopedro/vrp/mpdptw/MPDPTW_ALNS_TEST.java
@@ -19,7 +19,7 @@ public class MPDPTW_ALNS_TEST {
 
     @Test
     public void mpdptw_large_4_25_1_test() {
-        ALNS alns = new ALNS(rootDirectory, "l_4_25_1.txt", maxIterations, new Random(1));
+        ALNS alns = new ALNS(rootDirectory, "l_4_400_1.txt", maxIterations, new Random(1));
         alns.execute();
     }
 
