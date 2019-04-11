628c3e2d2a5bfa1cc14461ce116f57ed9b6d4536
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2019-04-11 20:26:03 -0300

    Add sequential infeasible construction

---
+++ src/main/java/com/github/schmittjoaopedro/vrp/mpdptw/aco/SequentialInfeasible.java
@@ -0,0 +1,303 @@
+package com.github.schmittjoaopedro.vrp.mpdptw.aco;
+
+import com.github.schmittjoaopedro.vrp.mpdptw.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class SequentialInfeasible implements SolutionBuilder {
+
+    private ProblemInstance instance;
+
+    private Random random;
+
+    private MMAS mmas;
+
+    private List<Solution> antPopulation;
+
+    public final double weight1 = 0.3;
+
+    public final double weight2 = 0.3;
+
+    public final double weight3 = 0.3;
+
+    public final double weight4 = 0.1;
+
+    @Override
+    public void init(ProblemInstance instance, Random random, MMAS mmas) {
+        this.instance = instance;
+        this.random = random;
+        this.mmas = mmas;
+        this.antPopulation = mmas.getAntPopulation();
+    }
+
+    @Override
+    public void constructSolutions() {
+        int vehicle;
+        Request request;
+        double vehicleCapacity;
+        NextClient nextVisit;
+        int currentPosition;
+        double departureTimes[] = new double[instance.getNumNodes()];
+        double demands[] = new double[instance.getNumNodes()];
+        ArrayList<Double> capacity = new ArrayList<>();
+        for (Solution ant : antPopulation) {
+            SolutionUtils.antEmptyMemory(ant, instance);
+        }
+        for (int k = 0; k < antPopulation.size(); k++) {// For each ant
+            vehicle = 0;
+            Solution ant = antPopulation.get(k);
+            ant.toVisit--; // The depot is visited
+            ant.tours.add(vehicle, new ArrayList<>());
+            ant.tours.get(vehicle).add(instance.getDepot().nodeId);
+            ant.requests.add(vehicle, new ArrayList<>());
+            ant.visited[instance.getDepot().nodeId] = true;
+            capacity.add(vehicle, instance.getVehicleCapacity());
+            currentPosition = 0;
+            while (ant.toVisit > 0) {
+                nextVisit = selectNextClient(ant, vehicle, currentPosition, capacity, departureTimes, demands);
+                if (nextVisit.nextClient != -1) { // a next visit node was found
+                    currentPosition++;
+                    ant.tours.get(vehicle).add(currentPosition, nextVisit.nextClient);
+                    ant.visited[nextVisit.nextClient] = true;
+                    ant.feasible &= nextVisit.feasible; // If in one move is infeasible, the full solution will be infeasible
+                    departureTimes[nextVisit.nextClient] = nextVisit.departureTime;
+                    demands[nextVisit.nextClient] = nextVisit.demand;
+                    ant.toVisit--;
+                    request = instance.getRequest(nextVisit.nextClient);
+                    if (request.isDeliver) {
+                        vehicleCapacity = capacity.get(vehicle) - request.demand; // Delivery demand is negative
+                        capacity.set(vehicle, vehicleCapacity);
+                    }
+                    if (!ant.requests.get(vehicle).contains(request.requestId)) {
+                        ant.requests.get(vehicle).add(request.requestId);
+                        vehicleCapacity = capacity.get(vehicle) + instance.getDelivery(request.requestId).demand; // Delivery demand is negative
+                        capacity.set(vehicle, vehicleCapacity);
+                    }
+                } else {
+                    ant.tours.get(vehicle).add(instance.getDepot().nodeId); // Finish previous route
+                    // Starts a new route
+                    vehicle++;
+                    currentPosition = 0;
+                    ant.tours.add(vehicle, new ArrayList<>());
+                    ant.tours.get(vehicle).add(instance.getDepot().nodeId);
+                    ant.requests.add(vehicle, new ArrayList<>());
+                    capacity.add(vehicle, instance.getVehicleCapacity());
+                }
+            }
+            ant.tours.get(vehicle).add(instance.getDepot().nodeId); // Finish last route
+        }
+        // Calculate fitness
+        for (Solution ant : antPopulation) {
+            instance.solutionEvaluation(ant);
+            if (ant.capacityPenalty > 0) {
+                throw new RuntimeException("Invalid capacity penaly!!");
+            }
+        }
+    }
+
+    public NextClient selectNextClient(Solution ant, int vehicle, int currIdx, ArrayList<Double> capacities, double[] departureTimes, double[] demands) {
+        Request request;
+        int currentCity = ant.tours.get(vehicle).get(currIdx);
+        NextClient nextClientHeuristic;
+        double sum = 0.0;
+        boolean hasRequest, valid;
+        ArrayList<NextClient> newRequestProbs = new ArrayList<>();
+        ArrayList<NextClient> currRequestProbs = new ArrayList<>();
+        ArrayList<NextClient> probs = new ArrayList<>();
+        for (int i = 0; i < instance.getNumNodes(); i++) {
+            if (!ant.visited[i] && isNextClientSupported(ant, capacities, vehicle, currentCity, i)) {
+                // Calculate heuristic value for the next client and its feasibility
+                nextClientHeuristic = HEURISTIC(ant, departureTimes, demands, vehicle, currentCity, i);
+                // If the vehicle has request, validate if the deliver can be executed after all pickups were made
+                request = instance.getRequest(i);
+                hasRequest = ant.requests.get(vehicle).contains(request.requestId);
+                valid = request.isPickup; // We assume that all pickups requests are valid a-priori
+                if (hasRequest && request.isDeliver) { // If the request is a delivery request we must to test if all pickups were made previously
+                    valid = true;
+                    for (Request req : instance.getPickups(request.requestId)) {
+                        if (!ant.visited[req.nodeId]) {
+                            valid = false;
+                            break;
+                        }
+                    }
+                }
+                // If the next client belongs to the current vehicle requests, add to new request probs
+                if (hasRequest && valid) {
+                    // [nextClient, heuristic, departureTime, demand, feasible, cumulativeCost]
+                    currRequestProbs.add(nextClientHeuristic);
+                } else if (nextClientHeuristic.feasible && valid) { // Else add to new request probs, but allow only new feasible requests
+                    newRequestProbs.add(nextClientHeuristic);
+                }
+            }
+        }
+
+        // Check if there is at least one unfeasible node in the request list
+        boolean currReqFeasible = true;
+        for (NextClient nextClient : currRequestProbs) {
+            if (!nextClient.feasible) { //unfeasible
+                currReqFeasible = false;
+                break;
+            }
+        }
+        // If are all feasible nodes in current request
+        if (currReqFeasible) {
+            // Select only feasible moves
+            for (NextClient nextClient : currRequestProbs) {
+                sum += nextClient.heuristic; //heuristic
+                nextClient.cumulativeCost = sum;
+                probs.add(nextClient);
+            }
+            for (NextClient nextClient : newRequestProbs) {
+                if (nextClient.feasible) {
+                    sum += nextClient.heuristic;
+                    nextClient.cumulativeCost = sum;
+                    probs.add(nextClient);
+                }
+            }
+        } else {
+            for (NextClient nextClient : currRequestProbs) {
+                sum += nextClient.heuristic;
+                nextClient.cumulativeCost = sum;
+                probs.add(nextClient);
+            }
+        }
+        // If this route is done, returns a flag indicating no more clients to add
+        if (probs.isEmpty()) {
+            NextClient nextClient = new NextClient();
+            nextClient.nextClient = -1;
+            return nextClient;
+        }
+        for (NextClient nextClient : probs) {
+            nextClient.cumulativeCost = (nextClient.cumulativeCost / sum) * 100.0;
+        }
+        // Roulette wheel selection
+        double rand = random.nextDouble() * 100.0;
+        int selected = 0;
+        while (selected != probs.size() - 1) {
+            if (rand <= probs.get(selected).cumulativeCost) {
+                break;
+            }
+            selected++;
+        }
+        return probs.get(selected);
+    }
+
+    public boolean isNextClientSupported(Solution ant, ArrayList<Double> capacities, int vehicle, int currentCity, int nextCity) {
+        boolean supported;
+        int requestId = instance.getRequestId(nextCity);
+        // Check if the request is already allocated in the vehicle and must be attended
+        if (ant.requests.get(vehicle).contains(requestId) || nextCity == instance.getDepot().nodeId) {
+            supported = true;
+        } else {
+            // Check if the vehicle supports the demand of the new request
+            supported = capacities.get(vehicle) + instance.getDelivery(requestId).demand > 0;
+            // Check if all requests from the nextCity request are feasbile from the current point
+            for (Request req : instance.getPickups(requestId)) {
+                supported &= instance.isFeasible(currentCity, req.nodeId);
+            }
+            if (currentCity != 0) { // Depot never will be feasibly with delivery points
+                supported &= instance.isFeasible(currentCity, instance.getDelivery(requestId).nodeId);
+            }
+        }
+        return supported;
+    }
+
+    public NextClient HEURISTIC(Solution ant, double[] departureTimes, double[] demands, int vehicle, int currentCity, int nextCity) {
+        NextClient nextClient = new NextClient();
+        nextClient.nextClient = nextCity;
+        nextClient.feasible = true;
+        Request nextReq = instance.getRequest(nextCity);
+        double demand;
+        double departureTime;
+        double timeCostDelivery;
+        double timeCostDepot;
+        // Calculate cost to next client
+        departureTime = departureTimes[currentCity] + instance.dist(currentCity, nextCity);
+        departureTime = Math.max(departureTime, nextReq.twStart);
+        demand = demands[currentCity] + nextReq.demand;
+        if (departureTime > nextReq.twEnd || demand > instance.getVehicleCapacity()) {
+            nextClient.feasible = false;
+        }
+        departureTime += nextReq.serviceTime;
+        nextClient.departureTime = departureTime;
+        nextClient.demand = demand;
+        // Is fair to calculate the cost to the delivery before to calculate the cost to depot
+        if (nextReq.isPickup) {
+            Request depotReq = instance.getDelivery(nextReq.requestId);
+            timeCostDelivery = departureTime + instance.dist(nextCity, depotReq.nodeId);
+            timeCostDelivery = Math.max(depotReq.twStart, timeCostDelivery);
+            if (timeCostDelivery > depotReq.twEnd) {
+                nextClient.feasible = false;
+            }
+            timeCostDelivery += depotReq.serviceTime;
+            // Calculate cost to depot
+            timeCostDepot = timeCostDelivery + instance.dist(depotReq.nodeId, instance.getDepot().nodeId);
+            timeCostDepot = Math.max(timeCostDepot, instance.getDepot().twStart);
+            if (timeCostDepot > instance.getDepot().twEnd) {
+                nextClient.feasible = false;
+            }
+        } else { // Our current node is the delivery point
+            // Calculate cost to depot
+            timeCostDepot = departureTime + instance.dist(nextCity, instance.getDepot().nodeId);
+            timeCostDepot = Math.max(timeCostDepot, instance.getDepot().twStart);
+            if (timeCostDepot > instance.getDepot().twEnd) {
+                nextClient.feasible = false;
+            }
+        }
+        // Calculate heuristic Racula
+        double deliveryUrgency = nextReq.twEnd - departureTime;
+        double timeDifference = departureTime - departureTimes[currentCity];
+        double futureCost = calculateFutureCost(ant, vehicle, currentCity, nextCity);
+        double cost = 1.0 / (weight1 * instance.dist(currentCity, nextCity) + weight2 * timeDifference + weight3 * futureCost + weight4 * deliveryUrgency);
+
+        //double cost = 1.0 / (0.3 * instance.distances[currentCity][nextCity] + 0.3 * futureCost + 0.3 * deliveryUrgency);
+
+        // Original heuristic transition rule from Dorigo
+        nextClient.heuristic = Math.pow(cost, mmas.getBeta()) * Math.pow(mmas.getPheromoneNodes()[currentCity][nextCity], mmas.getAlpha());
+
+        // Transition rule proposed by Afshar
+        //nextClient.heuristic = (beta * cost) + (alpha * pheromoneNodes[currentCity][nextCity]);
+
+        // Only pheromone
+        //nextClient.heuristic = Math.pow(pheromoneNodes[currentCity][nextCity], alpha);
+
+        return nextClient;
+    }
+
+    public double calculateFutureCost(Solution ant, int vehicle, int currentCity, int nextCity) {
+        double cost = 0.0;
+        for (int reqId : ant.requests.get(vehicle)) {
+            for (Request req : instance.getPickups(reqId)) {
+                if (!ant.visited[req.nodeId]) {
+                    cost += instance.dist(currentCity, req.nodeId);
+                }
+            }
+            if (!ant.visited[instance.getDelivery(reqId).nodeId]) {
+                cost += instance.dist(currentCity, instance.getDelivery(reqId).nodeId);
+            }
+        }
+        int reqId = instance.getRequestId(nextCity);
+        if (!ant.visitedRequests[reqId]) {
+            for (Request req : instance.getPickups(reqId)) {
+                cost += instance.dist(currentCity, req.nodeId);
+            }
+            cost += instance.dist(currentCity, instance.getDelivery(reqId).nodeId);
+        }
+        return cost;
+    }
+
+    @Override
+    public void onSearchControlExecute() {
+    }
+
+    private class NextClient {
+        int nextClient = 0;
+        double heuristic = 0.0;
+        double departureTime = 0.0;
+        double demand = 0.0;
+        boolean feasible = false;
+        double cumulativeCost = 0.0;
+    }
+}
--- src/test/java/com/github/schmittjoaopedro/vrp/mpdptw/MPDPTW_MMAS_TEST.java
+++ src/test/java/com/github/schmittjoaopedro/vrp/mpdptw/MPDPTW_MMAS_TEST.java
@@ -3,6 +3,7 @@ package com.github.schmittjoaopedro.vrp.mpdptw;
 import com.github.schmittjoaopedro.tsp.tools.IterationStatistic;
 import com.github.schmittjoaopedro.tsp.utils.Maths;
 import com.github.schmittjoaopedro.vrp.dvrptw.DVRPTW_ACS_Test;
+import com.github.schmittjoaopedro.vrp.mpdptw.aco.SequentialInfeasible;
 import org.junit.Test;
 
 import java.nio.file.Paths;
@@ -82,7 +83,7 @@ public class MPDPTW_MMAS_TEST {
     }
 
     @Test
-    public void mpdptw_w_4_100_1_test() {
+    public void mpdptw_sequential_feasible_test() {
         Solver solver = new Solver(rootDirectory, "w_4_100_1.txt", 10, seed, 0.8, 1, true);
         solver.run();
         solver.setParallel(false);
@@ -97,6 +98,25 @@ public class MPDPTW_MMAS_TEST {
         assertThat(iterationStatistics.get(7).toString()).isEqualTo("IT. 8       BSF: 14562.20   BSF. SD: 0.00    IT. WORST: 19298.51    IT. BEST: 15273.61    IT. MEAN: 17480.74    IT. SD: 1044.38   BRANCH FACTOR: 0.990     DIV: 0.90      ");
         assertThat(iterationStatistics.get(8).toString()).isEqualTo("IT. 9       BSF: 14562.20   BSF. SD: 0.00    IT. WORST: 20845.94    IT. BEST: 15145.02    IT. MEAN: 18015.22    IT. SD: 1063.92   BRANCH FACTOR: 0.990     DIV: 0.91      ");
         assertThat(iterationStatistics.get(9).toString()).isEqualTo("IT. 10      BSF: 14562.20   BSF. SD: 0.00    IT. WORST: 19624.36    IT. BEST: 15865.56    IT. MEAN: 17712.94    IT. SD: 972.92    BRANCH FACTOR: 0.990     DIV: 0.90      ");
+    }
+
+    @Test
+    public void mpdptw_sequential_infeasible_test() {
+        Solver solver = new Solver(rootDirectory, "w_4_100_1.txt", 10, seed, 0.8, 1, true);
+        solver.setSolutionBuilderClass(SequentialInfeasible.class);
+        solver.run();
+        solver.setParallel(false);
+        List<IterationStatistic> iterationStatistics = solver.getIterationStatistics();
+        assertThat(iterationStatistics.get(0).toString()).isEqualTo("IT. 1       BSF: 22477.53   BSF. SD: 0.00    IT. WORST: 26579.34    IT. BEST: 22477.53    IT. MEAN: 24359.21    IT. SD: 977.59    BRANCH FACTOR: 0.990     DIV: 0.99      PEN. RATE: 1.00    FEASIBLE: F");
+        assertThat(iterationStatistics.get(1).toString()).isEqualTo("IT. 2       BSF: 21308.95   BSF. SD: 0.00    IT. WORST: 26133.12    IT. BEST: 21308.95    IT. MEAN: 24064.62    IT. SD: 969.63    BRANCH FACTOR: 1.875     DIV: 0.98      PEN. RATE: 1.00    FEASIBLE: F");
+        assertThat(iterationStatistics.get(2).toString()).isEqualTo("IT. 3       BSF: 21308.95   BSF. SD: 0.00    IT. WORST: 26641.34    IT. BEST: 21393.64    IT. MEAN: 23997.98    IT. SD: 1178.31   BRANCH FACTOR: 0.990     DIV: 0.94      PEN. RATE: 1.00    FEASIBLE: F");
+        assertThat(iterationStatistics.get(3).toString()).isEqualTo("IT. 4       BSF: 20812.26   BSF. SD: 0.00    IT. WORST: 25290.99    IT. BEST: 20812.26    IT. MEAN: 23275.98    IT. SD: 1081.86   BRANCH FACTOR: 1.452     DIV: 0.82      PEN. RATE: 1.00    FEASIBLE: F");
+        assertThat(iterationStatistics.get(4).toString()).isEqualTo("IT. 5       BSF: 20262.05   BSF. SD: 0.00    IT. WORST: 24131.28    IT. BEST: 20262.05    IT. MEAN: 21908.96    IT. SD: 870.48    BRANCH FACTOR: 1.337     DIV: 0.62      PEN. RATE: 1.00    FEASIBLE: F");
+        assertThat(iterationStatistics.get(5).toString()).isEqualTo("IT. 6       BSF: 19886.22   BSF. SD: 0.00    IT. WORST: 23692.17    IT. BEST: 19886.22    IT. MEAN: 20858.37    IT. SD: 789.40    BRANCH FACTOR: 1.327     DIV: 0.35      PEN. RATE: 1.00    FEASIBLE: F");
+        assertThat(iterationStatistics.get(6).toString()).isEqualTo("IT. 7       BSF: 19015.52   BSF. SD: 0.00    IT. WORST: 22283.93    IT. BEST: 19015.52    IT. MEAN: 20336.24    IT. SD: 666.96    BRANCH FACTOR: 1.231     DIV: 0.26      PEN. RATE: 1.00    FEASIBLE: F");
+        assertThat(iterationStatistics.get(7).toString()).isEqualTo("IT. 8       BSF: 19015.52   BSF. SD: 0.00    IT. WORST: 21508.53    IT. BEST: 19015.52    IT. MEAN: 19684.69    IT. SD: 786.59    BRANCH FACTOR: 0.990     DIV: 0.21      PEN. RATE: 1.00    FEASIBLE: F");
+        assertThat(iterationStatistics.get(8).toString()).isEqualTo("IT. 9       BSF: 19015.52   BSF. SD: 0.00    IT. WORST: 21949.73    IT. BEST: 19015.52    IT. MEAN: 19128.16    IT. SD: 447.91    BRANCH FACTOR: 0.990     DIV: 0.04      PEN. RATE: 1.00    FEASIBLE: F");
+        assertThat(iterationStatistics.get(9).toString()).isEqualTo("IT. 10      BSF: 19015.52   BSF. SD: 0.00    IT. WORST: 19844.10    IT. BEST: 19015.52    IT. MEAN: 19038.50    IT. SD: 124.77    BRANCH FACTOR: 0.990     DIV: 0.01      PEN. RATE: 1.00    FEASIBLE: F");
     }
 
     @Test
