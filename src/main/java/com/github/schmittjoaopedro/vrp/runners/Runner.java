60065e3a44570b75461b6473abd685985a71a965
João Pedro Schmitt <jschmitt@weg.net>
2019-05-10 08:43:12 -0300

    - add results of MPDPTW - add test runner

---
+++ src/main/java/com/github/schmittjoaopedro/vrp/runners/Runner.java
@@ -0,0 +1,53 @@
+package com.github.schmittjoaopedro.vrp.runners;
+
+import com.github.schmittjoaopedro.statistic.ExperimentResultWriter;
+import com.github.schmittjoaopedro.statistic.GlobalStatistics;
+import com.github.schmittjoaopedro.statistic.IterationStatistic;
+import com.github.schmittjoaopedro.statistic.TrialExecutor;
+import com.github.schmittjoaopedro.vrp.mpdptw.DataReader;
+import com.github.schmittjoaopedro.vrp.mpdptw.ProblemInstance;
+import com.github.schmittjoaopedro.vrp.mpdptw.alns.ALNS;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Runner {
+
+    private static int maxIterations = 100000;
+
+    public static void main(String[] args) throws Exception {
+        String inputFolder = args[0];
+        String outputFolder = args[1];
+        ExperimentResultWriter resultWriter = new ExperimentResultWriter();
+        resultWriter.initializeALNS(maxIterations);
+        for (File file : new File(inputFolder).listFiles()) {
+            executeInstance(resultWriter, file, outputFolder);
+        }
+        resultWriter.compileResults(outputFolder, "RESULTS.csv");
+    }
+
+    private static void executeInstance(ExperimentResultWriter resultWriter, File file, String outputFolder) throws Exception {
+        int trials = 10;
+        List<Runnable> algs = new ArrayList<>();
+        for (int i = 0; i < trials; i++) {
+            ProblemInstance instance = DataReader.getPdptwInstance(file);
+            ALNS alns = new ALNS(instance, maxIterations, new Random(i));
+            alns.setGenerateFile(false);
+            alns.setShowLog(false);
+            algs.add(alns);
+        }
+        TrialExecutor trialExecutor = new TrialExecutor();
+        System.out.println("Starting " + file.getName());
+        trialExecutor.runAlgorithms(algs);
+        List<List<IterationStatistic>> results = new ArrayList<>();
+        for (int i = 0; i < trials; i++) {
+            results.add(((ALNS) algs.get(i)).getIterationStatistics());
+        }
+        GlobalStatistics globalStatistics = trialExecutor.getGlobalStatistics(results);
+        List<IterationStatistic> unifiedStatistics = trialExecutor.getUnifiedStatistics(results);
+        resultWriter.computeResultsALNS(outputFolder, algs.get(0).getClass().getSimpleName(), file.getName(), globalStatistics, unifiedStatistics);
+    }
+
+}
