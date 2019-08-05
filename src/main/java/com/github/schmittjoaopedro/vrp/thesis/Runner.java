e168edd7b923d130becb51e4fa612a2f9324a3f5
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2019-08-04 22:00:25 -0300

    instances 400

--- src/main/java/com/github/schmittjoaopedro/vrp/thesis/Runner.java
+++ src/main/java/com/github/schmittjoaopedro/vrp/thesis/Runner.java
@@ -12,19 +12,10 @@ public class Runner {
     private static int maxIterations = 25000;
 
     private static final String pdptw100Directory;
-
     private static final String pdptw200Directory;
+    private static final String pdptw400Directory;
 
     private static String[] instances_100 = {
-            "lc1_2_1", "lc1_2_2", "lc1_2_3", "lc1_2_4", "lc1_2_5", "lc1_2_6", "lc1_2_7", "lc1_2_8", "lc1_2_9", "lc1_2_10",
-            "lc2_2_1", "lc2_2_2", "lc2_2_3", "lc2_2_4", "lc2_2_5", "lc2_2_6", "lc2_2_7", "lc2_2_8", "lc2_2_9", "lc2_2_10",
-            "lr1_2_1", "lr1_2_2", "lr1_2_3", "lr1_2_4", "lr1_2_5", "lr1_2_6", "lr1_2_7", "lr1_2_8", "lr1_2_9", "lr1_2_10",
-            "lr2_2_1", "lr2_2_2", "lr2_2_3", "lr2_2_4", "lr2_2_5", "lr2_2_6", "lr2_2_7", "lr2_2_8", "lr2_2_9", "lr2_2_10",
-            "lrc1_2_1", "lrc1_2_2", "lrc1_2_3", "lrc1_2_4", "lrc1_2_5", "lrc1_2_6", "lrc1_2_7", "lrc1_2_8", "lrc1_2_9", "lrc1_2_10",
-            "lrc2_2_1", "lrc2_2_2", "lrc2_2_3", "lrc2_2_4", "lrc2_2_5", "lrc2_2_6", "lrc2_2_7", "lrc2_2_8", "lrc2_2_9", "lrc2_2_10"
-    };
-
-    private static String[] instances_200 = {
             "lc101", "lc102", "lc103", "lc104", "lc105", "lc106", "lc107", "lc108", "lc109",
             "lc201", "lc202", "lc203", "lc204", "lc205", "lc206", "lc207", "lc208",
             "lr101", "lr102", "lr103", "lr104", "lr105", "lr106", "lr107", "lr108", "lr109", "lr110", "lr111", "lr112",
@@ -33,14 +24,33 @@ public class Runner {
             "lrc201", "lrc202", "lrc203", "lrc204", "lrc205", "lrc206", "lrc207", "lrc208"
     };
 
+    private static String[] instances_200 = {
+            "lc1_2_1", "lc1_2_2", "lc1_2_3", "lc1_2_4", "lc1_2_5", "lc1_2_6", "lc1_2_7", "lc1_2_8", "lc1_2_9", "lc1_2_10",
+            "lc2_2_1", "lc2_2_2", "lc2_2_3", "lc2_2_4", "lc2_2_5", "lc2_2_6", "lc2_2_7", "lc2_2_8", "lc2_2_9", "lc2_2_10",
+            "lr1_2_1", "lr1_2_2", "lr1_2_3", "lr1_2_4", "lr1_2_5", "lr1_2_6", "lr1_2_7", "lr1_2_8", "lr1_2_9", "lr1_2_10",
+            "lr2_2_1", "lr2_2_2", "lr2_2_3", "lr2_2_4", "lr2_2_5", "lr2_2_6", "lr2_2_7", "lr2_2_8", "lr2_2_9", "lr2_2_10",
+            "lrc1_2_1", "lrc1_2_2", "lrc1_2_3", "lrc1_2_4", "lrc1_2_5", "lrc1_2_6", "lrc1_2_7", "lrc1_2_8", "lrc1_2_9", "lrc1_2_10",
+            "lrc2_2_1", "lrc2_2_2", "lrc2_2_3", "lrc2_2_4", "lrc2_2_5", "lrc2_2_6", "lrc2_2_7", "lrc2_2_8", "lrc2_2_9", "lrc2_2_10"
+    };
+
+    private static String[] instances_400 = {
+            "lc1_4_1", "lc1_4_2", "lc1_4_3", "lc1_4_4", "lc1_4_5", "lc1_4_6", "lc1_4_7", "lc1_4_8", "lc1_4_9", "lc1_4_10",
+            "lc2_4_1", "lc2_4_2", "lc2_4_3", "lc2_4_4", "lc2_4_5", "lc2_4_6", "lc2_4_7", "lc2_4_8", "lc2_4_9", "lc2_4_10",
+            "lr1_4_1", "lr1_4_2", "lr1_4_3", "lr1_4_4", "lr1_4_5", "lr1_4_6", "lr1_4_7", "lr1_4_8", "lr1_4_9", "lr1_4_10",
+            "lr2_4_1", "lr2_4_2", "lr2_4_3", "lr2_4_4", "lr2_4_5", "lr2_4_6", "lr2_4_7", "lr2_4_8", "lr2_4_9", "lr2_4_10",
+            "lrc1_4_1", "lrc1_4_2", "lrc1_4_3", "lrc1_4_4", "lrc1_4_5", "lrc1_4_6", "lrc1_4_7", "lrc1_4_8", "lrc1_4_9", "lrc1_4_10",
+            "lrc2_4_1", "lrc2_4_2", "lrc2_4_3", "lrc2_4_4", "lrc2_4_5", "lrc2_4_6", "lrc2_4_7", "lrc2_4_8", "lrc2_4_9", "lrc2_4_10"
+    };
+
     static {
         pdptw100Directory = Paths.get(Runner.class.getClassLoader().getResource("pdp_100").getFile().substring(1)).toString();
         pdptw200Directory = Paths.get(Runner.class.getClassLoader().getResource("pdp_200").getFile().substring(1)).toString();
+        pdptw400Directory = Paths.get(Runner.class.getClassLoader().getResource("pdp_400").getFile().substring(1)).toString();
     }
 
     public static void main(String[] args) throws Exception {
-        for (String instance : instances_100) {
-            executeProblemSolver(pdptw100Directory, instance);
+        for (String instance : instances_400) {
+            executeProblemSolver(pdptw400Directory, instance);
         }
     }
 
